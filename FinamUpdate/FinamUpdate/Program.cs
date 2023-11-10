466f8703a81aedba20be01f9e515dcad4c9b7d32
Павел <apaex@ngs.ru>
2023-11-10 18:28:10 +0600

    Диалог выбора файла

--- FinamUpdate/FinamUpdate/Program.cs
+++ FinamUpdate/FinamUpdate/Program.cs
@@ -10,7 +10,20 @@ namespace QuotesUpdate
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
+            ApplicationConfiguration.Initialize();            
+            
+            Settings.Default.Reload();
+            if (!File.Exists(Settings.Default.InputFile))
+            {
+                OpenFileDialog ofd = new OpenFileDialog();
+                ofd.Filter = "Excel files (*.xlsx)|*.xlsx|All files (*.*)|*.*";
+                ofd.Title = "Выберите файл пассивной накопительной стратегии";
+                if (ofd.ShowDialog() != DialogResult.OK)
+                    return;
+                Settings.Default.InputFile = ofd.FileName;
+                Settings.Default.Save();
+            }
+
             Application.Run(new Form1());
         }
     }
